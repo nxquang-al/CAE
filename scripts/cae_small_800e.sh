332baf2cdf3fe4ca62115a09f576901900be5da1
ngxquang <nq23092016@gmail.com>
2023-06-13 12:06:15 +0700

    Change running script

--- scripts/cae_small_800e.sh
+++ scripts/cae_small_800e.sh
@@ -1,26 +1,16 @@
-tmp_my_name=${0##*/}
-my_name=${tmp_my_name%.*}
+my_name="Synthetic-IHR-NomDB"
 
-OUTPUT_DIR='./output/'$my_name
-DATA_PATH="../Dataset/Synthetic-IHR-NomDB/generated_augmented/generated_augmented/train"
-TOKENIZER_PATH="./tokenizer-weights/dall_e_tokenizer_weights/"
-
-ADDRESS=ADDR_FOR_THIS_MACHINE                                                                                 
-NNODES=4     
-RANK=RANK_FOR_THIS_MACHINE                                                                                                                        
+OUTPUT_DIR='./output/Synthetic-IHR-NomDB'
+DATA_PATH="../Dataset/Synthetic-IHR-NomDB/generated_augmented/train"
+TOKENIZER_PATH="./tokenizer-weights/dall_e_tokenizer_weight/"
+                                                                                                                 
 
 # ============================ pretraining ============================
-OMP_NUM_THREADS=1 python -m torch.distributed.launch \
-  --nproc_per_node=8 \
-  --nnodes=$NNODES \
-  --node_rank=$RANK \
-  --master_addr=$ADDRESS \
-  --master_port=8899 \
-  tools/run_pretraining.py \
+python tools/run_pretraining.py \
   --data_path ${DATA_PATH} \
   --output_dir ${OUTPUT_DIR} \
   --model cae_small_patch48x4_48x432_8k_vocab --discrete_vae_weight_path ${TOKENIZER_PATH} \
-  --batch_size 4096 --lr 1.5e-4 --warmup_epochs 0.5 --epochs 10 \
+  --batch_size 128 --lr 1.5e-3 --warmup_epochs 1 --epochs 10 \
   --clip_grad 3.0 --layer_scale_init_value 0.1 \
   --imagenet_default_mean_and_std \
   --color_jitter 0 \
@@ -35,5 +25,3 @@ OMP_NUM_THREADS=1 python -m torch.distributed.launch \
   --regressor_depth 4 \
   --decoder_depth 4 \
   --align_loss_weight 0.05
-  # --align_loss_weight 2 \
-  # --num_mask_patches 98 \
